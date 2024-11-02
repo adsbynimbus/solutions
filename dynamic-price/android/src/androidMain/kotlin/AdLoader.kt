fd96338b78fa053d2c3ae8d8d328510e909ade8a
Jason Sznol <jason@nimbus.co>
2024-11-01 20:52:04 -0400

    Added Dynamic Price implementation

---
+++ dynamic-price/android/src/androidMain/kotlin/AdLoader.kt
@@ -0,0 +1,104 @@
+package adsbynimbus.solutions.dynamicprice.app
+
+import android.content.Context
+import android.util.Log
+import androidx.core.view.doOnAttach
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.adsbynimbus.google.handleEventForNimbus
+import com.adsbynimbus.openrtb.enumerations.Position
+import com.adsbynimbus.openrtb.request.Format
+import com.adsbynimbus.request.NimbusRequest
+import com.adsbynimbus.request.NimbusRequest.Companion.forBannerAd
+import com.amazon.device.ads.DTBAdRequest
+import com.amazon.device.ads.DTBAdSize
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import com.google.android.gms.ads.admanager.AdManagerAdView
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+import java.lang.System.currentTimeMillis
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.measureTimedValue
+
+lateinit var appContext: Context
+
+val adCache = mutableMapOf<String, DynamicPriceAd>()
+
+val preloadScope = MainScope() + CoroutineName("AdPreLoader")
+
+fun preloadBanner(
+    nimbusRequest: NimbusRequest = forBannerAd("Top Banner", Format.BANNER_320_50, Position.HEADER),
+    amazonRequest: () -> DTBAdRequest = {
+        DTBAdRequest(appContext).apply {
+            setSizes(DTBAdSize(320, 50, BuildConfig.AMAZON_BANNER_SLOT_ID))
+        }
+    },
+    bidders: List<Bidder<*>> = listOf(NimbusBidder(nimbusRequest), ApsBidder(amazonRequest)),
+    timeout: Duration = 1000.milliseconds
+) = DynamicPriceAd(
+    adUnitId = BuildConfig.ADMANAGER_ADUNIT_ID,
+    adSize = AdSize.BANNER,
+    bidders = bidders,
+    preloadBids = preloadScope.async { bidders.auction(timeout) }
+)
+
+class DynamicPriceAd(
+    adUnitId: String,
+    adSize: AdSize,
+    bidders: Collection<Bidder<*>>,
+    context: Context = appContext,
+    preloadBids: Deferred<List<Bid<*>>>? = null,
+) {
+    private var lastRequestTime = 0L
+    /** Must attach a listener prior to attaching to a parent view */
+    val adView by lazy {
+        AdManagerAdView(context).apply {
+            setAdUnitId(adUnitId)
+            setAdSize(adSize)
+            setAppEventListener { name, info -> handleEventForNimbus(name, info) }
+            doOnAttach {
+                val lifecycleOwner = findViewTreeLifecycleOwner() ?: throw Exception()
+                lifecycleOwner.lifecycleScope.launch {
+                    // If a preloaded bid exists load it when attached to the view hierarchy
+                    preloadBids?.let {
+                        lastRequestTime = currentTimeMillis()
+                        val (bids, auctionTime) = measureTimedValue { it.await() }
+                        val request = AdManagerAdRequest.Builder().apply {
+                            bids.forEach { it.applyTargeting(this) }
+                        }.build()
+                        loadAd(request)
+                        Log.i("Ads", "Preload Time: $auctionTime")
+                    }
+                    // Start normal refreshing tied using the LifecycleScope
+                    try {
+                        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                            /* The while loop here enables refreshing the ad tied to the lifecycle */
+                            while (isActive) {
+                                delay(30_000 - (currentTimeMillis() - lastRequestTime))
+                                waitUntilVisible()
+                                if (!isActive) return@repeatOnLifecycle
+                                lastRequestTime = currentTimeMillis()
+                                val adRequest = AdManagerAdRequest.Builder().apply {
+                                    bidders.auction().forEach { it.applyTargeting(this) }
+                                }
+                                if (isActive) loadAd(adRequest.build())
+                            }
+                        }
+                    } finally {
+                        destroy()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ dynamic-price/android/src/androidMain/kotlin/Bidders.kt
@@ -0,0 +1,67 @@
+package adsbynimbus.solutions.dynamicprice.app
+
+import com.adsbynimbus.NimbusAdManager
+import com.adsbynimbus.lineitem.DEFAULT_BANNER
+import com.adsbynimbus.lineitem.applyDynamicPrice
+import com.adsbynimbus.request.NimbusRequest
+import com.adsbynimbus.request.NimbusResponse
+import com.amazon.device.ads.DTBAdRequest
+import com.amazon.device.ads.DTBAdResponse
+import com.amazon.device.ads.DTBAdUtil
+import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import kotlinx.coroutines.async
+import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.yield
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+/** Interface representing a bidder that runs before the AdManager request */
+sealed interface Bidder<T> {
+    suspend fun fetchBid(): Bid<T>
+}
+
+/** A response from a successful call to a [Bidder] */
+@JvmInline
+value class Bid<T>(val response: T)
+
+/** Runs a parallel auction with enforced timeout for a collection of bidders */
+suspend inline fun Collection<Bidder<*>>.auction(
+    timeout: Duration = 3000.milliseconds,
+): List<Bid<*>> = supervisorScope {
+    val bidRequests = map {
+        async { withTimeout(timeout) { it.fetchBid() } }
+    }
+    yield()
+    bidRequests.mapNotNull {
+        runCatching { it.await() }.getOrNull()
+    }
+}
+
+/** A global instance of the NimbusAdManager for loading all Nimbus ads */
+val nimbusAdManager by lazy { NimbusAdManager() }
+
+/** Price Mapping used by Nimbus, should be replaced by Publisher specific price mapping */
+val linearPriceMapping = DEFAULT_BANNER
+
+/** Loads a bid from Nimbus using the global NimbusAdManager instance */
+@JvmInline
+value class NimbusBidder(private val adRequest: NimbusRequest) : Bidder<NimbusResponse> {
+    // appContext is defined in AdLoader.kt and set in the AdInitializer
+    override suspend fun fetchBid(): Bid<NimbusResponse> =
+        Bid(nimbusAdManager.makeRequest(appContext, adRequest))
+}
+
+/** Loads a bid from Amazon using a coroutine [loadAsync] defined in AsyncUtils */
+@JvmInline
+value class ApsBidder(private val adRequest: () -> DTBAdRequest) : Bidder<DTBAdResponse> {
+    override suspend fun fetchBid(): Bid<DTBAdResponse> = Bid(adRequest().loadAsync())
+}
+
+/** Applies targeting values from a Bid to an AdManagerAdRequest.Builder */
+inline fun <reified T> Bid<out T>.applyTargeting(request: AdManagerAdRequest.Builder) {
+    when (response) {
+        is NimbusResponse -> response.applyDynamicPrice(request, linearPriceMapping)
+        is DTBAdResponse -> DTBAdUtil.INSTANCE.loadDTBParams(request, response)
+    }
+}
