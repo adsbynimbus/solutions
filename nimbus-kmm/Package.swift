2fe42292ef44913fe0c19b5f45c3fb09f63cfae2
Jason Sznol <jason@nimbus.co>
2025-02-25 12:42:13 -0500

    Added Nimbus KMM implementation

---
+++ nimbus-kmm/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "NimbusKMM",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: "NimbusKMM", type: .dynamic, targets: ["NimbusKMM"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/adsbynimbus/nimbus-ios-sdk.git", from: "2.25.5"),
+    ],
+    targets: [
+        .target(
+            name: "NimbusKMM",
+            dependencies: [
+                .product(name: "NimbusKit", package: "nimbus-ios-sdk"),
+                .product(name: "NimbusRenderVASTKit", package: "nimbus-ios-sdk"),
+            ])
+    ]
+)
