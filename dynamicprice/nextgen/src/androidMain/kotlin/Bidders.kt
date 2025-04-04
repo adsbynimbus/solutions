60b679a4cc0f75310756942245fc1dde747c4ab5
Jason Sznol <jason@nimbus.co>
2025-04-04 12:24:59 -0400

    Added Next Gen Dynamic Price implementation

---
+++ dynamicprice/nextgen/src/androidMain/kotlin/Bidders.kt
@@ -0,0 +1,82 @@
+package adsbynimbus.solutions.dynamicprice.nextgen
+
+import com.adsbynimbus.NimbusAdManager
+import com.adsbynimbus.lineitem.DEFAULT_BANNER
+import com.adsbynimbus.request.NimbusRequest
+import com.adsbynimbus.request.NimbusResponse
+import com.amazon.device.ads.AdType
+import com.amazon.device.ads.DTBAdRequest
+import com.amazon.device.ads.DTBAdResponse
+import com.google.android.libraries.ads.mobile.sdk.common.BaseAdRequestBuilder
+import com.google.android.libraries.ads.mobile.sdk.common.BaseRequest
+import kotlinx.coroutines.async
+import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.yield
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+/** Interface representing a bidder that runs before the AdManager request */
+sealed interface Bidder<T> {
+    suspend fun fetchBid(): Bid<T>
+}
+
+/** A response from a successful call to a [Bidder] */
+@JvmInline
+value class Bid<T>(val response: T)
+
+/** Runs a parallel auction with enforced timeout for a collection of bidders */
+suspend inline fun Collection<Bidder<*>>.auction(
+    timeout: Duration = 3000.milliseconds,
+): List<Bid<*>> = supervisorScope {
+    val bidRequests = map {
+        async { withTimeout(timeout) { it.fetchBid() } }
+    }
+    yield()
+    bidRequests.mapNotNull {
+        runCatching { it.await() }.getOrNull()
+    }
+}
+
+/** A global instance of the NimbusAdManager for loading all Nimbus ads */
+val nimbusAdManager by lazy { NimbusAdManager() }
+
+/** Price Mapping used by Nimbus, should be replaced by Publisher specific price mapping */
+val linearPriceMapping = DEFAULT_BANNER
+
+/** Loads a bid from Nimbus using the global NimbusAdManager instance */
+@JvmInline
+value class NimbusBidder(private val adRequest: NimbusRequest) : Bidder<NimbusResponse> {
+    // appContext is defined in AdLoader.kt and set in the AdInitializer
+    override suspend fun fetchBid(): Bid<NimbusResponse> =
+        Bid(nimbusAdManager.makeRequest(appContext, adRequest))
+}
+
+/** Loads a bid from Amazon using a coroutine [loadAsync] defined in AsyncUtils */
+@JvmInline
+value class ApsBidder(private val adRequest: () -> DTBAdRequest) : Bidder<DTBAdResponse> {
+    override suspend fun fetchBid(): Bid<DTBAdResponse> = Bid(adRequest().loadAsync())
+}
+
+/** Applies targeting values from a Bid to an AdManagerAdRequest.Builder */
+inline fun <reified T> Bid<out T>.applyTargeting(request: BaseAdRequestBuilder<*>) {
+    when (response) {
+        is NimbusResponse -> response.applyDynamicPrice(request, linearPriceMapping)
+        is DTBAdResponse if response.adCount > 0 -> response.adManagerParams.forEach {
+            request.putCustomTargeting(it.key, it.value)
+        }
+    }
+}
+
+inline val DTBAdResponse.adManagerParams: Map<String, List<String>>
+    get() = when (dtbAds.first().dtbAdType) {
+        AdType.VIDEO -> defaultVideoAdsRequestCustomParams.mapValues { listOf(it.value) }
+        else -> defaultDisplayAdsRequestCustomParams
+    }
+
+inline val BaseRequest.keyValues: String
+    get() = customTargeting.entries.joinToString(
+        separator = ",\n",
+        prefix = "[\n",
+        postfix = "]",
+    )
