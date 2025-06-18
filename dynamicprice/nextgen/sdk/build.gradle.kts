b72891e0a6a044fa6a100bfd4bab00c7ba664f87
Jason Sznol <jason@nimbus.co>
2025-06-17 21:23:54 -0400

    Added Dokka 2.0 documentation plugin

--- dynamicprice/nextgen/sdk/build.gradle.kts
+++ dynamicprice/nextgen/sdk/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka.javadoc)
 }
 
 kotlin {
@@ -25,4 +27,33 @@ kotlin {
             implementation(libs.ads.google.nextgen)
         }
     }
+}
+
+dokka {
+    moduleName = "Dynamic Price NextGen"
+    dokkaGeneratorIsolation = ClassLoaderIsolation()
+    dokkaSourceSets {
+        named("androidMain") {
+            perPackageOption {
+                matchingRegex = """.*\.internal.*"""
+                suppress = true
+            }
+
+            sourceLink {
+                localDirectory = layout.projectDirectory.dir("src/$name/kotlin")
+                remoteLineSuffix = "#L"
+                remoteUrl("https://github.com/adsbynimbus/solutions/tree/main/dynamicprice/nextgen/sdk/src/$name/kotlin")
+            }
+        }
+    }
+}
+
+val dokkaJavadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaGeneratePublicationJavadoc.flatMap { it.outputDirectory })
+}
+
+val dokkaHtmlJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("html-doc")
+    from(tasks.dokkaGeneratePublicationHtml.flatMap { it.outputDirectory })
 }
