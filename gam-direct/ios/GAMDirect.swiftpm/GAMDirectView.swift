1eb0db5c8d9bd4b84b4a014563124865e26744af
Jason Sznol <jason@nimbus.co>
2025-12-16 13:22:11 -0500

    Added AdMob Bidding and removed extraneous sizes

--- gam-direct/ios/GAMDirect.swiftpm/GAMDirectView.swift
+++ gam-direct/ios/GAMDirect.swiftpm/GAMDirectView.swift
@@ -8,23 +8,22 @@
 @preconcurrency import DTBiOSSDK
 import GoogleMobileAds
 @preconcurrency import NimbusKit
+@preconcurrency import NimbusAdMobKit
 
 fileprivate let refreshInterval: TimeInterval = 30
 
 @MainActor
 func exampleBanner(
-    googleAdUnitId: String,
+    gamDirectAdUnitId: String,
     amazonSlotId: String,
+    admobBiddingAdUnitId: String,
 ) -> GAMDirectView {
     return .init(
-        adUnitId: googleAdUnitId,
+        directAdUnitId: gamDirectAdUnitId,
         adManagerAdSizes: [currentOrientationInlineAdaptiveBanner(width: 320)],
         apsSlotId: amazonSlotId,
         apsSize: .MREC,
-        nimbusSizes: [
-            NimbusAdFormat(width: 300, height: 250),
-            NimbusAdFormat(width: 620, height: 250)
-        ]
+        admobBiddingAdUnitId: admobBiddingAdUnitId
     )
 }
 
@@ -35,6 +34,7 @@ public class GAMDirectView : UIView {
     private let adManagerAdSizes: [AdSize]
     private let apsSlotId: String
     private let apsSize: APSAdFormat
+    private let admobBiddingId: String
     private let nimbusSizes: Set<NimbusAdFormat>
     private var lastRequestTime: Date = Date.distantPast
     private var refreshTask: Task<Void, Error>?
@@ -46,16 +46,17 @@ public class GAMDirectView : UIView {
 
     /// Set to true for banner ads if they refresh when not on the screen
     public var useOnScreenCheck = false
-    
+
     public init(
-        adUnitId: String,
+        directAdUnitId: String,
         adManagerAdSizes: [AdSize],
         apsSlotId: String,
         apsSize: APSAdFormat,
-        nimbusSizes: Set<NimbusAdFormat>,
+        admobBiddingAdUnitId: String,
+        nimbusSizes: Set<NimbusAdFormat> = [],
     ) {
         self.adLoader = AdLoader(
-            adUnitID: adUnitId,
+            adUnitID: directAdUnitId,
             rootViewController: nil,
             adTypes: [ .adManagerBanner ],
             options: nil,
@@ -64,16 +65,17 @@ public class GAMDirectView : UIView {
         self.apsSlotId = apsSlotId
         self.apsSize = apsSize
         self.nimbusSizes = nimbusSizes
+        self.admobBiddingId = admobBiddingAdUnitId
         super.init(frame: .zero)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     public func loadAd() async {
         lastRequestTime = Date()
-        
+
         // If the ad load is successful there will be 2 children with the old ad at index 0 which we should remove
         defer {
             if subviews.count > 1 {
@@ -84,10 +86,9 @@ public class GAMDirectView : UIView {
         }
 
         let adManagerRequest = AdManagerRequest()
-        // This key value will be used for targeting in GAM in lieu of different ad units and
-        // you should check with your ad ops team on what the actual value should be
+        // Any targeting values used for Ad Manager should be appended here
         adManagerRequest.customTargeting = ["nimbus":"true"]
-        
+
         if let directAd = try? await adLoader.loadAd(request: adManagerRequest, sizes: adManagerAdSizes) {
             directAd.delegate = googleDelegate
             addSubview(directAd)
@@ -98,18 +99,20 @@ public class GAMDirectView : UIView {
             ])
             return
         }
-        
-        
+
+
         let nimbusRequest = NimbusRequest.forBannerAd(position: adLoader.adUnitID, format: .interstitialPortrait)
         nimbusRequest.impressions[0].banner?.formats = nimbusSizes
-        nimbusRequest.impressions[0].banner?.blockedCreativeAttributes = [.userInteractive]
-        
+
         let apsAdRequest = APSAdRequest(slotUUID: apsSlotId, adNetworkInfo: APSAdNetworkInfo(networkName: .nimbus))
         apsAdRequest.setAdFormat(apsSize)
         if let apsAd = try? await apsAdRequest.loadAd() {
             nimbusRequest.addAPSResponse(apsAd)
         }
-        
+
+        // Add AdMob Bidding
+        nimbusRequest.withAdMobBanner(adUnitId: admobBiddingId)
+
         // Uses an async implementation of the manual request and render flow
         // NimbusAdManager.showAd() = NimbusRequestManager.performRequest + Nimbus.load(ad)
         if let nimbusAd = try? await nimbusRequest.fetchAd() {
@@ -121,26 +124,26 @@ public class GAMDirectView : UIView {
             )
         }
     }
-    
+
     // Handles starting and stopping the automatic refresh of ads
     public override func willMove(toWindow newWindow: UIWindow?) {
         removeVisibilityListeners()
-        
+
         guard let newWindow = newWindow else {
             self.refreshTask?.cancel()
             return
         }
-        
+
         onVisibilityChanged(newWindow) { [unowned self] isVisible in
             Task { @MainActor in isVisible ? self.startRefresh() : self.refreshTask?.cancel() }
         }
-        
+
         startRefresh()
     }
-    
+
     private func startRefresh() {
         guard refreshTask == nil || refreshTask?.isCancelled == true else { return }
-        
+
         refreshTask = Task {
             while !Task.isCancelled {
                 // If not on screen, sleep for 200 milliseconds and then check again
@@ -151,7 +154,7 @@ public class GAMDirectView : UIView {
 
                 await Task.sleep(seconds: refreshInterval - Date().timeIntervalSince(lastRequestTime))
                 guard !Task.isCancelled else { break }
-                
+
                 await loadAd()
             }
         }
@@ -159,13 +162,13 @@ public class GAMDirectView : UIView {
 }
 
 public extension UIView {
-    
+
     var isOnScreen: Bool {
         guard let window else { return false }
-    
+
         return !window.bounds.intersection(convert(bounds, to: nil)).isEmpty
     }
-    
+
     @inlinable
     func removeVisibilityListeners() -> Void {
         NotificationCenter.default.removeObserver(
@@ -177,7 +180,7 @@ public extension UIView {
         NotificationCenter.default.removeObserver(
             self, name: UIApplication.didEnterBackgroundNotification, object: UIApplication.shared)
     }
-    
+
     @inlinable
     func onVisibilityChanged(_ target: UIWindow,  _ onChange: @escaping @Sendable (Bool) -> Void) {
         NotificationCenter.default.addObserver(
@@ -201,7 +204,7 @@ public extension UIView {
             queue: nil
         ) { _ in onChange(false) }
     }
-    
+
     func parentViewController() -> UIViewController? {
         var responder: UIResponder? = self
         while !(responder is UIViewController) {
@@ -218,26 +221,26 @@ extension AdLoader {
     final class RequestListener : NSObject, Sendable, AdManagerBannerAdLoaderDelegate {
         nonisolated(unsafe) var continuation: UnsafeContinuation<AdManagerBannerView, Error>?
         let sizes: [AdSize]
-        
+
         init(sizes: [AdSize]) {
             self.sizes = sizes
         }
-        
+
         func adLoader(_ adLoader: AdLoader, didReceive bannerView: AdManagerBannerView) {
             continuation?.resume(returning: bannerView)
             continuation = nil
         }
-        
+
         func adLoader(_ adLoader: AdLoader, didFailToReceiveAdWithError error: any Error) {
             continuation?.resume(throwing: error)
             continuation = nil
         }
-        
+
         func validBannerSizes(for adLoader: AdLoader) -> [NSValue] {
             return sizes.map { nsValue(for: $0) }
         }
     }
-    
+
     @MainActor
     func loadAd(request: AdManagerRequest, sizes: [AdSize]) async throws -> AdManagerBannerView {
         let listener = RequestListener(sizes: sizes)
@@ -283,12 +286,12 @@ extension NimbusRequest {
     // Helper method for appending APS params using the new APSAd methods
     public func addAPSResponse(_ response: APSAd) {
         guard let targeting = response.customTargeting else { return }
-        
+
         var ext = impressions[0].extensions ?? [:]
         ext["aps"] = NimbusCodable([targeting])
         impressions[0].extensions = ext
     }
-        
+
     final class RequestListener : NimbusRequestManagerDelegate, Sendable {
 
         nonisolated(unsafe) var continuation: UnsafeContinuation<NimbusAd, Error>?
