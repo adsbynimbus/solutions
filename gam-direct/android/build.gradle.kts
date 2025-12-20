5a8e89333ae1e577eb3287abae5787ebec91c7e1
Jason Sznol <jason@nimbus.co>
2025-12-20 17:24:11 -0500

    save

---
+++ gam-direct/android/build.gradle.kts
@@ -0,0 +1,74 @@
+import org.jetbrains.kotlin.gradle.dsl.*
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.app)
+}
+
+val githubActions = providers.environmentVariable("GITHUB_ACTIONS")
+
+kotlin {
+    androidTarget {
+        compilations.configureEach {
+            compileTaskProvider.configure {
+                compilerOptions.jvmTarget = JvmTarget.fromTarget(libs.versions.android.jvm.get())
+            }
+        }
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.bundles.androidx)
+            implementation(libs.bundles.gamdirect)
+            implementation(libs.kotlin.coroutines)
+        }
+    }
+}
+
+androidComponents.beforeVariants {
+    it.enable = it.name.contains("release", ignoreCase = true) || !githubActions.isPresent
+}
+
+android {
+    compileSdk = libs.versions.android.sdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "adsbynimbus.solutions.gamdirect".also { namespace = it }
+        minSdk = libs.versions.android.min.get().toInt()
+        targetSdk = libs.versions.android.sdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+        manifestPlaceholders["appName"] = "Nimbus GAM Direct"
+        with(providers) {
+            manifestPlaceholders["gamAppId"] = gradleProperty("adsbynimbus.solutions.admanagerAppId").get()
+            buildConfigField("String", "API_KEY", "\"${gradleProperty("adsbynimbus.solutions.apiKey").get()}\"")
+            buildConfigField("String", "PUBLISHER_KEY", "\"${gradleProperty("adsbynimbus.solutions.publisherKey").get()}\"")
+            buildConfigField("String", "ADMANAGER_ADUNIT_ID", "\"${gradleProperty("adsbynimbus.solutions.admanagerAdUnitId").get()}\"")
+            buildConfigField("String", "AMAZON_APP_KEY", "\"${gradleProperty("adsbynimbus.solutions.amazonAppId").get()}\"")
+            buildConfigField("String", "AMAZON_BANNER_SLOT_ID", "\"${gradleProperty("adsbynimbus.solutions.amazonBannerSlotId").get()}\"")
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                layout.projectDirectory.file("src/androidMain/proguard-rules.txt"),
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.android.jvm.get())
+    }
+
+    packaging.resources {
+        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+}
---
+++ gam-direct/android/src/androidMain/kotlin/AdInitializer.kt
@@ -0,0 +1,36 @@
+package adsbynimbus.solutions.gamdirect
+
+import android.content.Context
+import android.util.Log
+import androidx.startup.Initializer
+import com.adsbynimbus.Nimbus
+import com.amazon.device.ads.AdRegistration
+import com.amazon.device.ads.MRAIDPolicy
+import com.google.android.gms.ads.MobileAds
+import kotlin.time.TimeSource.Monotonic
+import kotlin.time.measureTime
+
+class AdInitializer : Initializer<Unit> {
+    override fun create(context: Context) {
+        val nimbusStartup = measureTime {
+            Nimbus.initialize(context, BuildConfig.PUBLISHER_KEY, BuildConfig.API_KEY)
+            Nimbus.testMode = true
+        }
+
+        val amazonStartup = measureTime {
+            AdRegistration.getInstance(BuildConfig.AMAZON_APP_KEY, context)
+            AdRegistration.setMRAIDSupportedVersions(arrayOf("1.0", "2.0", "3.0"))
+            AdRegistration.setMRAIDPolicy(MRAIDPolicy.DFP)
+            AdRegistration.enableTesting(true)
+            //AdRegistration.enableLogging(true)
+        }
+
+        val googleStart = Monotonic.markNow()
+        MobileAds.initialize(context) {
+            val googleStartup = Monotonic.markNow() - googleStart
+            Log.i("Ads", "Nimbus: $nimbusStartup, Amazon: $amazonStartup, Google: $googleStartup")
+        }
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> = mutableListOf()
+}
