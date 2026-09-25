ac5aa4ea35d74e2343da3ea559ab6cfa61044c7a
Jason Sznol <jason@nimbus.co>
2026-09-25 06:54:17 -0400

    Set Kotlin language target to 2.2

--- sdk-extensions/android/admob/build.gradle.kts
+++ sdk-extensions/android/admob/build.gradle.kts
@@ -49,8 +49,8 @@ kotlin {
     }
 
     compilerOptions {
-        apiVersion = KotlinVersion.KOTLIN_2_0
-        languageVersion = KotlinVersion.KOTLIN_2_0
+        apiVersion = KotlinVersion.KOTLIN_2_2
+        languageVersion = KotlinVersion.KOTLIN_2_2
     }
 
     explicitApi()
